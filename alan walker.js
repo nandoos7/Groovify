cde79b6b0c2a30f87b9661a4e5b964a24ab036bb
Nandini Mahant <nandooshere07@gmail.com>
2024-12-09 09:03:24 +0530

    Add files via upload

---
+++ alan walker.js
@@ -0,0 +1,113 @@
+console.log("Welcome to Groovify");
+
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Alone", filePath: "Alan Walker/1.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "Darkside", filePath: "Alan Walker/2.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "Faded", filePath: "Alan Walker/3.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "Sing me to Sleep", filePath: "Alan Walker/4.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "The Spectre", filePath: "Alan Walker/5.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "Alone part 2", filePath: "Alan Walker/6.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "Sweet Dreams", filePath: "Alan Walker/7.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "Alone part 3", filePath: "Alan Walker/8.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "Barcelona", filePath: "Alan Walker/9.mp3", coverPath: "images/alanwalker.jpg"},
+    {songName: "On My Way", filePath: "Alan Walker/10.mp3", coverPath: "images/alanwalker.jpg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `Alan Walker/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `Alan Walker/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `Alan Walker/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+
+
+
